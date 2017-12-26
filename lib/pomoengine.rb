cce965e218eaa5f4ea90b2ad9de2bd352f7f7c4a
PharaohKJ <kato@phalanxware.com>
2017-12-26 23:54:11 +0900

    とりまCLI基礎。

--- lib/pomoengine.rb
+++ lib/pomoengine.rb
@@ -1,7 +1,10 @@
 require 'dotenv'
 Dotenv.load
+require 'thor'
+require 'mqtt'
 
 require 'pomoengine/version'
+require 'pomoengine/cli'
 
 module Pomoengine
   # Your code goes here...
---
+++ lib/pomoengine/cli.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+module Pomoengine
+  class CLI < Thor
+    desc "timer", "countdown timer and publish current"
+    def timer
+    end
+  end
+end
