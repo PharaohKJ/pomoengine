6e9aec7cde42bc23d75809f3a024b10bc6fa3c02
PharaohKJ <kato+github@phalanxware.com>
2018-01-20 06:27:50 +0900

    Feature/タイマー部分作成 (#1)
    * 実行結果を AWS の DynamoDB に書くようにした。
    たぶん認証はAWS SDKのものが使われているので動かすにはそれの設定がいる。
    
    * Ctrl-c で停止できるようにした。
    
    * 終了時には say コマンドを実行している(実質 macOSオンリー動作)

--- lib/pomoengine.rb
+++ lib/pomoengine.rb
@@ -1,7 +1,14 @@
 require 'dotenv'
 Dotenv.load
+require 'thor'
+require 'mqtt'
+require 'eventmachine'
+require 'date'
+
+require 'aws-sdk-dynamodb'
 
 require 'pomoengine/version'
+require 'pomoengine/cli'
 
 module Pomoengine
   # Your code goes here...
